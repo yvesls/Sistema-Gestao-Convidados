b3a0b21203f9bacb22cc07c18d140b38f4d752e5
Yves Lima Silva <yveslimasilva@gmail.com>
2022-05-03 17:31:04 -0300

    Realizei a instalação do eclipse, das dependências e do framework Spring

---
+++ gestao-festa/src/test/java/com/springboot/gestaofesta/GestaoFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gestaofesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
